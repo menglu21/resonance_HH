318e18c7f200538684824b9d83da4ba0ce7d3d58
Meng_Lu <1501110102@pku.edu.cn>
2023-11-04 21:36:10 +0100

    Update mc_cfg.py

--- crab/mc_cfg.py
+++ crab/mc_cfg.py
@@ -13,7 +13,7 @@ config.JobType.psetName = 'PSet.py'
 config.JobType.scriptExe = 'dummy'
 # hadd nano will not be needed once nano tools are in cmssw
 config.JobType.inputFiles = ['crab_script.py', '../scripts/haddnano.py','keep_and_drop.txt']
-config.JobType.sendPythonFolder = True
+#config.JobType.sendPythonFolder = True
 
 config.section_("Data")
 config.Data.inputDataset = 'dummy'
